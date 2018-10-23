07ecfe9e484a815186d250fe5184854297d736bf
Anwesh43 <anweshthecool0@gmail.com>
2018-10-23 09:48:55 +0530

    adding BiEndLineCircleView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbiendlinecircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbiendlinecircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbiendlinecircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.biendcirclelineview.BiEndLineCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiEndLineCircleView.create(this)
     }
 }
