a58bcd4fb5f2ca8ceb13f284af5e26c58dc76f14
Anwesh43 <anweshthecool0@gmail.com>
2018-10-23 08:57:55 +0530

    importing libraries and defining nodes

--- biendcirclelineview/src/main/java/com/anwesh/uiprojects/biendcirclelineview/BiEndLineCircleView.kt
+++ biendcirclelineview/src/main/java/com/anwesh/uiprojects/biendcirclelineview/BiEndLineCircleView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.biendcirclelineview
 /**
  * Created by anweshmishra on 23/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+
+val nodes : Int = 5
+
