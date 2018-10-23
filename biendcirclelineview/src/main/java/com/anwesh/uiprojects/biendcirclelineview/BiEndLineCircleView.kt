3f4bbb64c360e906af545ed3bcee4646b1e0d081
Anwesh43 <anweshthecool0@gmail.com>
2018-10-23 09:21:52 +0530

    adding BELCNode class

--- biendcirclelineview/src/main/java/com/anwesh/uiprojects/biendcirclelineview/BiEndLineCircleView.kt
+++ biendcirclelineview/src/main/java/com/anwesh/uiprojects/biendcirclelineview/BiEndLineCircleView.kt
@@ -102,4 +102,49 @@ class BiEndLineCircleView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BELCNode(var i : Int, val state : State = State()) {
+
+        private var next : BELCNode? = null
+
+        private var prev : BELCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BELCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBELCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BELCNode {
+            var curr : BELCNode? = this.prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
